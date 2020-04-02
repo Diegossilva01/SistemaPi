81b9c10f9be984b799f789b55f33bed5c8f40df2
diego <diego@DESKTOP-14VP9MM.Home>
2020-04-02 13:13:07 -0300

    tela login pronta

---
+++ src/visao/telaLogin.java
@@ -0,0 +1,152 @@
+
+package visao;
+
+
+import java.sql.*;
+
+
+import javax.swing.JOptionPane;
+
+
+public class telaLogin extends javax.swing.JFrame {
+
+    
+   
+    public telaLogin() {
+        initComponents();
+       
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        usuario = new javax.swing.JTextField();
+        senha = new javax.swing.JPasswordField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        Acessar = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 204, 255));
+        getContentPane().setLayout(null);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/LogoSample_ByTailorBrands (3).jpg"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(130, 0, 260, 160);
+        getContentPane().add(usuario);
+        usuario.setBounds(190, 190, 240, 30);
+        getContentPane().add(senha);
+        senha.setBounds(190, 230, 240, 30);
+
+        jLabel3.setText("Senha");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(130, 230, 60, 14);
+
+        jLabel2.setText("Usuario");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(120, 200, 50, 14);
+
+        Acessar.setText("Entar");
+        Acessar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AcessarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Acessar);
+        Acessar.setBounds(270, 280, 80, 30);
+
+        jButton2.setText("Sair");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(360, 280, 70, 30);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/fundo branco 1000.jpg"))); // NOI18N
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 0, 560, 400);
+
+        jLabel5.setText("jLabel5");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(160, 150, 34, 14);
+
+        setSize(new java.awt.Dimension(515, 433));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void AcessarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AcessarActionPerformed
+
+        
+     if(usuario.getText().equals("admin")&& senha.getText().equals("1234")){
+            
+            JOptionPane.showConfirmDialog(null, " senha correta !!!");
+        }
+        
+        else {
+            JOptionPane.showMessageDialog(null, "semha incorreta !!!");
+       }
+        
+        
+        
+    }//GEN-LAST:event_AcessarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(telaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(telaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(telaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(telaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new telaLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Acessar;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPasswordField senha;
+    private javax.swing.JTextField usuario;
+    // End of variables declaration//GEN-END:variables
+}
